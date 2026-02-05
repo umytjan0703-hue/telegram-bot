f4ae0600997e706bf457179d117f2616596c9a94
umytjan0703-hue <umytjan07.03@gmail.com>
2026-02-05 23:36:12 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "✅ Bot işleýär!")
+
+bot.polling(non_stop=True)
